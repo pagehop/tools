7f527f578c49cc5886bf199a949f1272702950ee
nicroto <nikolay@tsenkov.net>
2015-01-05 17:06:56 +0200

    Fix: fuzzy: potentially calls pagehop.finish() twice;
    Fix: fuzzy: hint warning.

--- fuzzy/tool.js
+++ fuzzy/tool.js
@@ -7,27 +7,26 @@ var currentResults = pagehop.getCurrentResults(),
 
 if ( !searchStr ) {
 	pagehop.finish( currentResults );
-	return;
+} else {
+	searchStr = searchStr.replace( /(\s)|(\t)/g, "" );
+
+	var options = {
+		pre: '<b>',
+		post: '</b>',
+
+		extract: function(item) {
+			return item.text;
+		}
+	};
+
+	var filtered = fuzzy.filter( searchStr, currentResults, options );
+
+	// Map the results to the html we want generated
+	var results = filtered.map(function(entry){
+		var originalItem = entry.original;
+		originalItem.displayText = entry.string;
+		return originalItem;
+	} );
+
+	pagehop.finish( results );
 }
-
-searchStr = searchStr.replace( /(\s)|(\t)/g, "" );
-
-var options = {
-	pre: '<b>',
-	post: '</b>',
-
-	extract: function(item) {
-		return item.text;
-	}
-};
-
-var filtered = fuzzy.filter( searchStr, currentResults, options );
-
-// Map the results to the html we want generated
-var results = filtered.map(function(entry){
-	var originalItem = entry.original;
-	originalItem.displayText = entry.string;
-	return originalItem;
-} );
-
-pagehop.finish( results );
